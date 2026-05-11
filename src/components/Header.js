16de987cf1b1a65c7fa52c96b374fdcd3497efd1
Shrishti <shrishtiojha1619@gmail.com>
2026-05-12 00:06:54 +0530

    photo and name display post sign up or sign in

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,13 +23,14 @@ const Header = () => {
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-04-27/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="Netflix Logo"
       />
-
-      <div className="flex p=2">
-        <img className="w-12 h-12" alt="usericon" src={user.photoURL}></img>
-        <button onClick={handleSignOut} className="font-bold text-white">
-          (Sign Out)
-        </button>
-      </div>
+      {user && (
+        <div className="flex p=2">
+          <img className="w-12 h-12" alt="usericon" src={user.photoURL}></img>
+          <button onClick={handleSignOut} className="font-bold text-white">
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
   );
 };
