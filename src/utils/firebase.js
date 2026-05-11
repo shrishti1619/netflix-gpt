d748b1f3205e86271da0567b7b100ee087867b11
Shrishti <shrishtiojha1619@gmail.com>
2026-05-11 23:59:44 +0530

    revoking public key

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,15 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDHuybFFPxNMDh2QHchH_J0v1H-RPEzN20",
-  authDomain: "netflix-gpt-1619.firebaseapp.com",
-  projectId: "netflix-gpt-1619",
-  storageBucket: "netflix-gpt-1619.firebasestorage.app",
-  messagingSenderId: "433181794677",
-  appId: "1:433181794677:web:260f36783ce5bd26fab33c",
-  measurementId: "G-FVEMEFPS3X",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
